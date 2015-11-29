7009af9ef30f20588a98d5b4235dec4d14b23953
Darrell Wright <dwright-git@dawdevel.ca>
2015-11-29 00:48:50 -0500

    Initial commit with sample tool

---
+++ WOLTool/WOLTool/frmMain.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WOLTool {
+	partial class frmMain {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing ) {
+			if( disposing && (components != null) ) {
+				components.Dispose( );
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent( ) {
+			this.txtMACAddress = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnWakeUp = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtMACAddress
+			// 
+			this.txtMACAddress.Location = new System.Drawing.Point(89, 6);
+			this.txtMACAddress.Name = "txtMACAddress";
+			this.txtMACAddress.Size = new System.Drawing.Size(153, 20);
+			this.txtMACAddress.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(71, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "MAC Address";
+			// 
+			// btnWakeUp
+			// 
+			this.btnWakeUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnWakeUp.Location = new System.Drawing.Point(218, 54);
+			this.btnWakeUp.Name = "btnWakeUp";
+			this.btnWakeUp.Size = new System.Drawing.Size(75, 23);
+			this.btnWakeUp.TabIndex = 2;
+			this.btnWakeUp.Text = "Wake Up";
+			this.btnWakeUp.UseVisualStyleBackColor = true;
+			this.btnWakeUp.Click += new System.EventHandler(this.btnWakeUp_Click);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(305, 89);
+			this.Controls.Add(this.btnWakeUp);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtMACAddress);
+			this.Name = "frmMain";
+			this.Text = "WOL Tool";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtMACAddress;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnWakeUp;
+	}
+}
+
---
+++ WOLTool/WOLTool/frmMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WOLTool {
+
+	public partial class frmMain: Form {
+
+		public frmMain( ) {
+			InitializeComponent( );
+		}
+
+		private static string cleanup_mac_string( string orig_mac_string ) {
+			const string valid_mac_address_chars = @"0123456789ABCDEF";
+			string mac_address = string.Empty;
+			foreach( var c in orig_mac_string.ToUpper( ) ) {
+				if( valid_mac_address_chars.Contains( c ) ) {
+					mac_address += c;
+				}
+			}
+			return mac_address;
+		}
+
+		private static byte str_to_nibble( char str_val ) {
+			if( str_val >= '0' && str_val <= '9' ) {
+				return (byte)(str_val - '0');
+			}
+			str_val |= ' ';
+			byte result = (byte)(str_val - 'a' + 10);
+			return result;
+		}
+
+		private static byte str_to_byte( char str_val1, char str_val2 ) {
+			byte p1 = (byte)(str_to_nibble( str_val1 ) << 4);
+			byte p2 = str_to_nibble( str_val2 );
+			byte result = (byte)(p1 | p2);
+			return result;
+		}
+
+		private static byte[] hex_string_to_byte( string hex_string ) {
+			System.Diagnostics.Debug.Assert( hex_string.Length % 2 == 0 );
+			var result = new byte[hex_string.Length / 2];
+			for( var i = 0; i < hex_string.Length; i += 2 ) {
+				result[i / 2] = str_to_byte( hex_string[i], hex_string[i + 1] );
+			}
+			return result;
+		}
+
+		private static string repeat_string( string value, int count ) {
+			var result = value;
+			for( int i = 1; i < count; ++i ) {
+				result += value;
+			}
+			return result;
+		}
+
+		private static void send_packet( string data_package ) {
+			var magic_packet = new System.Net.Sockets.UdpClient( );
+			try {
+				magic_packet.Connect( System.Net.IPAddress.Broadcast, 9 );
+				var dgram = hex_string_to_byte( data_package );
+				magic_packet.Send( dgram, dgram.Length );
+				magic_packet.Close( );
+			} catch( Exception e ) {
+				MessageBox.Show( string.Format( "Exception while sending packet: {0}", e.Message ) );
+			}
+		}
+
+		private void btnWakeUp_Click( object sender, EventArgs e ) {
+			var dgram = repeat_string( @"FF", 6 );
+			dgram += repeat_string( cleanup_mac_string( txtMACAddress.Text ), 16 );
+			send_packet( dgram );
+		}
+	}
+}
